46018a0303d1f350412c2115fa24f50d996a49ef
JoaoGeniselli <joao.g.geniselli@gmail.com>
2017-08-12 18:10:30 -0300

    adicionando novas implementações do android + atualização de protocolos + correções do web service

--- CatalogacaoWebService/src/main/java/br/com/jgeniselli/catalogacaoWS/model/DataUpdateVisit.java
+++ CatalogacaoWebService/src/main/java/br/com/jgeniselli/catalogacaoWS/model/DataUpdateVisit.java
@@ -1,6 +1,9 @@
 package br.com.jgeniselli.catalogacaoWS.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
@@ -30,6 +33,8 @@ public class DataUpdateVisit implements Serializable {
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date registerDate; // data
     
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
     @ManyToOne
     private User collector;
 
@@ -67,16 +72,8 @@ public class DataUpdateVisit implements Serializable {
         this.registerDate = registerDate;
     }
 
-    public void setObservation(String observation) {
-        this.notes = observation;
-    }
-
     public Date getCollectionDate() {
         return collectionDate;
-    }
-
-    public String getObservation() {
-        return notes;
     }
     
     public void addPhoto(Photo photo) {
