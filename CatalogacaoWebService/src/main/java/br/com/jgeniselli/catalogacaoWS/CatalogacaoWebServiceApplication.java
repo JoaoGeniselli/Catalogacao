b1bbb15ec766d27091c2c6cb6e901b61e9dfcbc2
JoaoGeniselli <joao.g.geniselli@gmail.com>
2017-09-21 15:22:08 -0300

    Configurações de deployable war.

--- CatalogacaoWebService/src/main/java/br/com/jgeniselli/catalogacaoWS/CatalogacaoWebServiceApplication.java
+++ CatalogacaoWebService/src/main/java/br/com/jgeniselli/catalogacaoWS/CatalogacaoWebServiceApplication.java
@@ -2,9 +2,16 @@ package br.com.jgeniselli.catalogacaoWS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CatalogacaoWebServiceApplication {
+public class CatalogacaoWebServiceApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CatalogacaoWebServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CatalogacaoWebServiceApplication.class, args);
