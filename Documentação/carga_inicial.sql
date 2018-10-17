e37aa1abf679257fcad2a401408da555480f0bfb
JoaoGeniselli <joao.g.geniselli@gmail.com>
2018-10-16 23:48:40 -0300

    removendo pasta de documentação

--- Documentação/carga_inicial.sql
+++ Documentação/carga_inicial.sql
@@ -10,8 +10,8 @@ insert into role(id, role) values(2, 'USER');
 select * from user;
 
 -- Criaçao do Admin 
-insert into user(id, enable, name, password, username) values (1, 1, 'Administrador', ***REMOVIDO***, 'admin');
--- Senha crua do admin: ***REMOVIDO***
+insert into user(id, enable, name, password, username) values (1, 1, 'Administrador', ***REMOVED***, 'admin');
+-- Senha crua do admin: ***REMOVED***
 
 -- Atribuiçao da autoridade ao admin
 insert into user_roles(user_id, roles_id) values (1,1);
